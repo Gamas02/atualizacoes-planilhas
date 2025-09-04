07ce25dd3d6fc90cccbdd270524c2ac082636622
Gamas02 <gamaliermatheus4@gmail.com>
2025-09-03 22:38:22 -0300

    Esboço dos componentes do site

--- atualizacoes-planilhas/src/App.js
+++ atualizacoes-planilhas/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Logo</p>
       </header>
+      <aside>
+        <div>bt1</div>
+        <div>bt2</div>
+        <div>bt3</div>
+      </aside>
+      <main>
+        <p>Tabela</p>
+      </main>
     </div>
   );
 }
