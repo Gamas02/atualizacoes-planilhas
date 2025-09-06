41b0526fbfae14b7eca0c5556df1b41881138cf5
Gamas02 <gamaliermatheus4@gmail.com>
2025-09-05 22:51:06 -0300

    NEW: implementação da função de importar csv e definição de outras functions

--- planilhas-code/src/App.js
+++ planilhas-code/src/App.js
@@ -1,4 +1,28 @@
+import { useState } from 'react';
 import './App.css';
+
+//importa arquivo csv
+const ImportaTabela = (e) => {
+  const file = e.target.files[0];
+
+  const reader = new FileReader();
+
+  reader.onload = (event) => { 
+    const text = event.target.result;
+    const rows = text.split("\n").map((r) => r.split(",")); 
+    console.log(rows); 
+  };
+}
+
+//chamar o arquivo csv em formato tabela
+function MostraTabela(){
+
+}
+
+//clicar no botao
+function handleClick(){
+
+}
 
 function App() {
   return (
